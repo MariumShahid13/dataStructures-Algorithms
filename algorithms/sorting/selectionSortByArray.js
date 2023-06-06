62dd2569cce6481f32f342c78b55e5d74b9ae59a
MariumGoraya13 <delivery.lead.dev@globaldrum.social>
2023-06-06 15:08:17 +0500

    sorted Array in ascending order using selection sort algorithm

---
+++ algorithms/sorting/selectionSortByArray.js
@@ -0,0 +1,24 @@
+const sortArrayInAscending = (inputArray, sortedArray) => {
+    if(inputArray.length === 0) {
+        return sortedArray
+    } else {
+        let min = Infinity
+        let minIndex = -1
+        /** check each element for min and update min and minIndex */
+        inputArray.forEach((ele, index) => {
+            if(ele < min) {
+                min = ele
+                minIndex = index
+            }
+        });
+        /** push min element from inputArray to sortedArray  */
+        sortedArray.push(min)
+        /** remove min element from list */
+        inputArray.splice(minIndex, 1)
+
+        return sortArrayInAscending(inputArray, sortedArray)
+    }
+}
+
+let result = sortArrayInAscending([100,2,-1,-4,71,10,4,1], [])
+console.log(result)
